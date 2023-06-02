23d3e40491ec2c2df6f4f1e0bf839483517c9caf
newfy2 <echo.shauqimaulana3@gmail.com>
2023-06-03 03:03:24 +0700

    perubahan

--- views/history.php
+++ views/history.php
@@ -1,3 +1,32 @@
+<?php
+//Terhubung ke Database
+$koneksi = mysqli_connect("localhost", "root", "", "latihan");
+
+//Mengirim Query
+$query = "SELECT * FROM riwayat";
+$result = mysqli_query($koneksi, $query);
+
+// Mengambil Data
+$data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+?>
+
+
 <div class="card">
-    INI HALAMAN HISTORY
+    <table>
+    <tr>
+        <th>Tanggal</th>
+        <th>No. Telpon</th>
+        <th>Nominal</th>
+        <th>Provider</th>
+    </tr>
+    <?php foreach ($data as $row): ?>
+  <tr>
+    <td><?php echo $row['tanggal']; ?></td>
+    <td><?php echo $row['no_kartu']; ?></td>
+    <td><?php echo $row['nominal']; ?></td>
+    <td><?php echo $row['provider']; ?></td>
+  </tr>
+  <?php endforeach; ?>
+    </table>
 </div>
--- views/layout.php
+++ views/layout.php
@@ -18,6 +18,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $pageTitle ?> - Pulsa</title>
     <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/history.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" 
     integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" 
     crossorigin="anonymous" referrerpolicy="no-referrer" />

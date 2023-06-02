7b2c5a1fbdfb8cfcd9d658c3b5c910783b02695c
ReizalPutra <mohreizal27@gmail.com>
2023-06-02 18:39:44 +0700

    fixing small things

--- views/send.php
+++ views/send.php
@@ -1,27 +1,28 @@
 <?php
 if (isset($_POST["submit"])) {
     if (post_tambah($_POST) > 0) {
-        echo " data berhasil ditambahkan";
+        echo "
+        <script>
+        alert('Top up Pulsa telah berhasil!!');
+        </script>";
     }else {
-        echo "data gagal";
+        echo "
+        <script>
+        alert('Top up Pulsa gagal!!');
+        </script>";
     }
 }
 ?>
-<!-- <style>
-    label{
-        background-color: blue;
-    }
-</style> -->
 <div class="card">
     
     <form action="" method="post" class="top-up">
         <h1>PULSA</h1>
-        <input type="text" placeholder="Masukan Nomor HP" name="no_kartu" class="input-topup1">
+        <input type="text" placeholder="Masukan Nomor HP" name="no_kartu" class="input-topup1" required>
         <br>
-        <input type="text" placeholder="Masukan Jumlah Pulsa" name="nominal" class="input-topup2">
+        <input type="text" placeholder="Masukan Jumlah Pulsa" name="nominal" class="input-topup2" required>
         <br>
         <div class="provider">
-        <input type="radio" id="xl" name="provider" value="xl" class="radio">
+        <input type="radio" id="xl" name="provider" value="xl" class="radio" required>
             <label for="xl" class="xl">XL</label>
         <input type="radio" id="telkomsel" name="provider" value="telkomsel" class="radio">
             <label for="telkomsel" class="telkomsel">Telkomsel</label>
